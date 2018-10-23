b98e5ddf3883edacbebb9bad950a70ce70808bd3
sushantsahoo <sushant.sahoo@mfine.co>
2018-10-23 08:07:20 +0530

    Workaround a wrong fling direction for inverted ScrollViews on Android

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
@@ -246,6 +246,20 @@ public class ReactScrollView extends ScrollView implements ReactClippingViewGrou
 
   @Override
   public void fling(int velocityY) {
+      
+      Log.d(
+            ReactConstants.TAG,
+            "FLING+++");
+      
+      // Workaround.
+      // On Android P if a ScrollView is inverted, we will get a wrong sign for
+      // velocityY (see https://issuetracker.google.com/issues/112385925).
+      // At the same time, mOnScrollDispatchHelper tracks the correct velocity direction.
+      //
+      // Hence, we can use the absolute value from whatever the OS gives
+      // us and use the sign of what mOnScrollDispatchHelper has tracked.
+      velocityY = (int)(Math.abs(velocityY) * Math.signum(mOnScrollDispatchHelper.getYFlingVelocity()));
+      
     if (mScroller != null) {
       // FB SCROLLVIEW CHANGE
 
